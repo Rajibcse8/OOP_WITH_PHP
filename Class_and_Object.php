52db7915dce09b984f3a1241ab7fd83bab152e3f
Rajibcse8 <73878397+Rajibcse8@users.noreply.github.com>
2020-12-14 23:05:10 +0600

    Create Class_and_Object.php

---
+++ Class_and_Object.php
@@ -0,0 +1,20 @@
+<?php
+   
+  
+       class Person{
+		  
+	   	  public $name;
+		  public $age;
+		  
+	  }
+	  
+	     $person1=new Person();
+	     $person1->name="Rajib";
+		 $person1->age=25;
+		
+		echo $person1->name." ".$person1->age;
+	
+	  
+  
+
+?>
